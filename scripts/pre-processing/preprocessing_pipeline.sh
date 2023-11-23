6404b1910ef454bac5cdf252d8850e96e91d13a6
LisaHemforth <91879834+LisaHemforth@users.noreply.github.com>
2023-11-23 15:19:00 +0100

    Update preprocessing_pipeline.sh

--- scripts/pre-processing/preprocessing_pipeline.sh
+++ scripts/pre-processing/preprocessing_pipeline.sh
@@ -1,5 +1,5 @@
 # The following scripts outlines the preprocessing steps for images to be used for automatic IHI prediction.
-# The first step is to use the clinica T1-volume processing step. Make sure your data is in the BIDS format. 
+# The first step is to use the clinica T1-volume processing. Make sure your data is in the BIDS format. 
 clinica run t1-volumee BIDS_DIRECTORY CAPS_DIRECTORY GROUP_LABEL -tsv TSV_FILE -wd WORKIN_DIRECTORY
 
 # We then extract the tensors from the greymatter maps which will be used for prediction using the previously defined ROI mask. The file for this mask can be found in this directory. 
