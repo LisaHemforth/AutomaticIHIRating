9bbb1cb26efc3aea5ce489aca309ef3d9d809589
LisaHemforth <91879834+LisaHemforth@users.noreply.github.com>
2023-11-23 15:18:24 +0100

    Update preprocessing_pipeline.sh

--- scripts/pre-processing/preprocessing_pipeline.sh
+++ scripts/pre-processing/preprocessing_pipeline.sh
@@ -3,4 +3,4 @@
 clinica run t1-volumee BIDS_DIRECTORY CAPS_DIRECTORY GROUP_LABEL -tsv TSV_FILE -wd WORKIN_DIRECTORY
 
 # We then extract the tensors from the greymatter maps which will be used for prediction using the previously defined ROI mask. The file for this mask can be found in this directory. 
-clinicadl extract roi CAPS_DIRECTORY custom --custom_suffix graymatter_space-Ixi549Space_modulated-off_probability.nii.gz --roi_custom_template MNI152NLin2009cSym --roi_list hippvol --subjects_sessions_tsv TSV_FILE --save_features --extract_json hippGM.json  --roi_uncrop_output
+clinicadl extract roi CAPS_DIRECTORY custom --custom_suffix normalized_space/*graymatter_space-Ixi549Space_modulated-off_probability.nii.gz --roi_custom_template MNI152NLin2009cSym --roi_list hippvol --subjects_sessions_tsv TSV_FILE --save_features --extract_json hippGM.json  --roi_uncrop_output
